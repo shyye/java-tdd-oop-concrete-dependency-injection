ae5fffb80d1b9e8c3f1bfb941891d1958b98bd7c
shyye <emma.art.dev@gmail.com>
2024-08-20 09:42:27 +0200

    Exercise core complete

--- src/main/java/com/booleanuk/core/Computer.java
+++ src/main/java/com/booleanuk/core/Computer.java
@@ -4,20 +4,31 @@ import java.util.ArrayList;
 
 public class Computer {
     public ArrayList<Game> installedGames = new ArrayList<>();
+    private PowerSupply poewerSupply;
 
-    public void turnOn() {
-        PowerSupply psu = new PowerSupply();
-        psu.turnOn();
+    public Computer(PowerSupply poewerSupply) {
+        this.poewerSupply = poewerSupply;
     }
 
-    public void installGame() {
-        Game game = new Game("Morrowind");
+    public Computer(PowerSupply poewerSupply, ArrayList<Game> preInstalled) {
+        this.poewerSupply = poewerSupply;
+        for (Game game : preInstalled) {
+            installGame(game.name);
+        }
+    }
+
+    public void turnOn() {
+        this.poewerSupply.turnOn();
+    }
+
+    public void installGame(String gameName) {
+        Game game = new Game(gameName);
         this.installedGames.add(game);
     }
 
-    public String playGame() {
+    public String playGame(String gameName) {
         for (Game g : this.installedGames) {
-            if (g.name.equals("Morrowind")) {
+            if (g.name.equals(gameName)) {
                 return g.start();
             }
         }
